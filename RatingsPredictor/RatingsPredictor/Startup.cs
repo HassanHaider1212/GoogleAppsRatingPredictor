dd7de21e3d912047b63f3defb8eab5d01bd26efb
Syed Hassan Haider <119353034+HassanHaider1212@users.noreply.github.com>
2023-11-22 03:09:24 +0500

    Rating Predictor Project Created.
    Rating Predictor Project Created. Now we will work on building our website.

---
+++ RatingsPredictor/RatingsPredictor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RatingsPredictor.Startup))]
+namespace RatingsPredictor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
